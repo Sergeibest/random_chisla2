9162f91baaba440915427b4581721c65e7a852d6
Sergeibest <51260293+Sergeibest@users.noreply.github.com>
2019-06-02 17:40:11 +0300

    Release

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Random;
+
+//улучшенная рулетка
+// не работает
+public class Main {
+
+    public static void main(String[] args) {
+        Random rulet = new Random();
+
+
+        int num[] = new int[33];
+
+        for (int i = 0; i > 1000; i++){
+            ++num[rulet.nextInt(33)];
+        }
+        System.out.println("Номер\tЧисло");
+
+        for ( int i = 0; i<num.length; i++){
+            System.out.println(i +"\t" + num[i]);
+        }
+    }
+}
